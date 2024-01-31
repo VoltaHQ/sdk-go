707541d332b6f04436863c6f311af887d0dec057
leo <6304835+ldcicconi@users.noreply.github.com>
2024-01-31 12:06:44 -0800

    use new executeBatch signature

--- vault.go
+++ vault.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/NuKeyHQ/sdk-go/contracts/EntryPoint"
+	account "github.com/NuKeyHQ/sdk-go/contracts/VoltaAccount"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -15,7 +16,7 @@ import (
 type VaultClient interface {
 	BuildExecuteUserOp(sender common.Address, call Call) (*UserOperation, error)
 	BuildExecuteUserOpFromTx(sender common.Address, tx *types.Transaction) (*UserOperation, error)
-	BuildExecuteBatchUserOp(sender common.Address, calls []Call) (*UserOperation, error)
+	BuildExecuteBatchUserOp(sender common.Address, batchCall account.ExecuteBatchCall) (*UserOperation, error)
 	BuildCustomUserOp(sender common.Address, callData []byte) (*UserOperation, error)
 	NextNonce(sender common.Address) (*big.Int, error)
 	SuggestGasTipCap(ctx context.Context) (*big.Int, error)
@@ -85,8 +86,8 @@ func (c vaultClient) BuildExecuteUserOpFromTx(sender common.Address, tx *types.T
 	})
 }
 
-func (c vaultClient) BuildExecuteBatchUserOp(sender common.Address, calls []Call) (*UserOperation, error) {
-	callData, err := accountABI.Pack("executeBatch", calls)
+func (c vaultClient) BuildExecuteBatchUserOp(sender common.Address, batchCall account.ExecuteBatchCall) (*UserOperation, error) {
+	callData, err := accountABI.Pack("executeBatch", batchCall)
 	if err != nil {
 		return nil, fmt.Errorf("failed to pack executeBatch call: %w", err)
 	}
